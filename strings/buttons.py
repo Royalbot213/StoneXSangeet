fc1cc991b67a0ad39f72211edb81631a679c8936
Royalbot213 <143687906+Royalbot213@users.noreply.github.com>
2025-07-26 17:49:38 +0530

    Update buttons.py

--- strings/buttons.py
+++ strings/buttons.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram import Client, filters, enums 
 
 class BUTTONS(object):
-    ABUTTON = [[InlineKeyboardButton("˹ sᴜᴘᴘᴏꝛᴛ ˼", url="https://t.me/+FnCOueyj4W1mNDZl")],[InlineKeyboardButton("˹ ᴜᴘᴅᴧᴛᴇ ˼", url="https://t.me/+YTsbDmCAJxVkMTc1"),
+    ABUTTON = [[InlineKeyboardButton("˹ sᴜᴘᴘᴏꝛᴛ ˼", url="https://t.me/+YTsbDmCAJxVkMTc1")],[InlineKeyboardButton("˹ ᴜᴘᴅᴧᴛᴇ ˼", url="https://t.me/+YTsbDmCAJxVkMTc1"),
     InlineKeyboardButton("˹ ᴧʟʟ ʙᴏᴛ ˼", url="https://t.me/+YTsbDmCAJxVkMTc1")],[InlineKeyboardButton("↺ ʙᴧᴄᴋ ↻", callback_data=f"settingsback_helper"),
     ]]
 
